6535ba63386ca6e91bc762bbf7183422a437e3ec
Jyrki Ruuskanen <jyrki.ruuskanen@finavia.fi>
2014-09-15 12:15:41 +0300

    Use Fuse versions, field value injection support, error handling in default route

--- src/main/java/com/github/yuruki/camel/runner/CamelRunnerMain.java
+++ src/main/java/com/github/yuruki/camel/runner/CamelRunnerMain.java
@@ -1,14 +1,21 @@
 package com.github.yuruki.camel.runner;
 
 import org.apache.camel.CamelContext;
+import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.properties.PropertiesComponent;
 import org.apache.camel.impl.DefaultCamelContext;
 import org.apache.camel.impl.DefaultCamelContextNameStrategy;
 import org.apache.camel.main.Main;
+import org.apache.camel.util.ReflectionHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
+import java.lang.reflect.Field;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.net.URI;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
@@ -18,6 +25,7 @@ public class CamelRunnerMain extends Main {
 
     private Logger log = LoggerFactory.getLogger(this.getClass());
     private String propertyPrefix = "";
+    private String camelContextId;
 
     private File propertiesFile;
     private Properties properties = new Properties();
@@ -67,9 +75,12 @@ public class CamelRunnerMain extends Main {
         // Set up properties
         setupPropertiesComponent(camelContext);
 
+        // Configure fields from properties
+        configure(camelContext, this, log);
+
         // Set up context
         try {
-            camelContext.setNameStrategy(new DefaultCamelContextNameStrategy(camelContext.resolvePropertyPlaceholders("{{camelContextId}}")));
+            camelContext.setNameStrategy(new DefaultCamelContextNameStrategy(camelContextId));
             camelContext.setUseMDCLogging(true);
             camelContext.setUseBreadcrumb(true);
         } catch (Exception e) {
@@ -85,6 +96,17 @@ public class CamelRunnerMain extends Main {
             }
         }
         return camelContext;
+    }
+
+    @Override
+    protected void postProcessCamelContext(CamelContext camelContext) throws Exception {
+        // try to load the route builders from the routeBuilderClasses
+        loadRouteBuilders(camelContext);
+        for (RouteBuilder routeBuilder : routeBuilders) {
+            // Configure route fields from properties
+            configure(camelContext, routeBuilder, log);
+            camelContext.addRoutes(routeBuilder);
+        }
     }
 
     private void setupPropertiesComponent(CamelContext camelContext) {
@@ -110,5 +132,56 @@ public class CamelRunnerMain extends Main {
         }
         pc.setLocations(locations.toArray(new String[locations.size()]));
         pc.setOverrideProperties(properties);
+    }
+
+    public static <T> void configure(CamelContext context, T target, Logger log) {
+        Class clazz = target.getClass();
+        for (Field field : clazz.getDeclaredFields()) {
+            try {
+                String propertyValue = context.resolvePropertyPlaceholders("{{" + field.getName() + "}}");
+                if (!propertyValue.isEmpty()) {
+                    // Try to convert the value and set the field
+                    Object convertedValue = convertValue(propertyValue, field.getGenericType());
+                    ReflectionHelper.setField(field, target, convertedValue);
+                    log.debug("Set field " + field.getName() + " with value " + propertyValue);
+                }
+            } catch (Exception e) {
+                log.debug("Field " + field.getName() + " skipped", e);
+            }
+        }
+    }
+
+    public static Object convertValue(String value, Type type) throws Exception {
+        Class<?> clazz = null;
+        if (type instanceof ParameterizedType) {
+            clazz = (Class<?>) ((ParameterizedType) type).getRawType();
+        } else if (type instanceof Class) {
+            clazz = (Class) type;
+        }
+        if (null != value) {
+            if (clazz.isInstance(value)) {
+                return value;
+            } else if (clazz == String.class) {
+                return value;
+            } else if (clazz == Boolean.class || clazz == boolean.class) {
+                return Boolean.parseBoolean(value);
+            } else if (clazz == Integer.class || clazz == int.class) {
+                return Integer.parseInt(value);
+            } else if (clazz == Long.class || clazz == long.class) {
+                return Long.parseLong(value);
+            } else if (clazz == Double.class || clazz == double.class) {
+                return Double.parseDouble(value);
+            } else if (clazz == File.class) {
+                return new File(value);
+            } else if (clazz == URI.class) {
+                return new URI(value);
+            } else if (clazz == URL.class) {
+                return new URL(value);
+            } else {
+                throw new IllegalArgumentException("Unknown type: "+ (clazz != null ? clazz.getName() : "null"));
+            }
+        } else {
+            return null;
+        }
     }
 }
--- src/main/java/com/github/yuruki/camel/runner/SimpleRouteBuilder.java
+++ src/main/java/com/github/yuruki/camel/runner/SimpleRouteBuilder.java
@@ -1,14 +1,89 @@
 package com.github.yuruki.camel.runner;
 
+import org.apache.camel.LoggingLevel;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.commons.lang.Validate;
 
 public class SimpleRouteBuilder extends RouteBuilder {
 
-    public void configure() {
+    private Integer maximumRedeliveries;
+    private Long redeliveryDelay;
+    private Double backOffMultiplier;
+    private Long maximumRedeliveryDelay;
+    private String camelRouteId = "simple-route";
+
+    @Override
+    public void configure() throws Exception {
+        checkProperties();
+
+        errorHandler(defaultErrorHandler()
+            .maximumRedeliveries(maximumRedeliveries)
+            .redeliveryDelay(redeliveryDelay)
+            .backOffMultiplier(backOffMultiplier)
+            .maximumRedeliveryDelay(maximumRedeliveryDelay));
 
         from("{{from}}")
-                .setBody(constant("foobar"))
-                .to("{{to}}");
+            .routeId(camelRouteId)
+            .onCompletion()
+                .to("direct:processCompletion")
+            .end()
+            .to("{{to}}");
+
+        from("direct:processCompletion")
+            .routeId(camelRouteId + ".completion")
+            .choice()
+                .when(simple("${exception} == null"))
+                    .log("{{messageOk}}")
+                .otherwise()
+                    .log(LoggingLevel.ERROR, "{{messageError}}")
+            .endChoice();
     }
 
+    public void checkProperties() {
+        Validate.notNull(maximumRedeliveries, "maximumRedeliveries property is not set");
+        Validate.notNull(redeliveryDelay, "redeliveryDelay property is not set");
+        Validate.notNull(backOffMultiplier, "backOffMultiplier property is not set");
+        Validate.notNull(maximumRedeliveryDelay, "maximumRedeliveryDelay property is not set");
+        Validate.notNull(camelRouteId, "camelRouteId property is not set");
+    }
+
+    public Integer getMaximumRedeliveries() {
+        return maximumRedeliveries;
+    }
+
+    public void setMaximumRedeliveries(Integer maximumRedeliveries) {
+        this.maximumRedeliveries = maximumRedeliveries;
+    }
+
+    public Long getRedeliveryDelay() {
+        return redeliveryDelay;
+    }
+
+    public void setRedeliveryDelay(Long redeliveryDelay) {
+        this.redeliveryDelay = redeliveryDelay;
+    }
+
+    public Double getBackOffMultiplier() {
+        return backOffMultiplier;
+    }
+
+    public void setBackOffMultiplier(Double backOffMultiplier) {
+        this.backOffMultiplier = backOffMultiplier;
+    }
+
+    public Long getMaximumRedeliveryDelay() {
+        return maximumRedeliveryDelay;
+    }
+
+    public void setMaximumRedeliveryDelay(Long maximumRedeliveryDelay) {
+        this.maximumRedeliveryDelay = maximumRedeliveryDelay;
+    }
+
+    public String getCamelRouteId() {
+        return camelRouteId;
+    }
+
+    public void setCamelRouteId(String camelRouteId) {
+        this.camelRouteId = camelRouteId;
+    }
 }
